72e25f6aa5f6bfa91bb1a96a3556cd913c2d4a8d
Martin Fides <fidesmartin@gmail.com>
2023-04-05 10:00:59 +0200

    Edit controller

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -6,10 +6,10 @@ from app.model.service import Service
 
 
 class Controller:
-    def __init__(self, service: Service, http_gateway: HttpGateway, soup: SoupGateway):
+    def __init__(self, service: Service, http_gateway: HttpGateway, soup_gateway: SoupGateway):
         self.service = service
         self.http_gateway = http_gateway
-        self.soup = soup
+        self.soup_gateway = soup_gateway
 
     @abc.abstractmethod
     def get_data(self) -> Service:
@@ -21,7 +21,7 @@ class ControllerIMPL(Controller):
         for company in self.service.companies:
             for shop in company.shops:
                 response = shop.get_response(self.http_gateway)
-                data = self.soup.find_all(response, shop.attributes)
+                data = self.soup_gateway.find_all(response, shop.attributes)
                 shop.parse_response(data)
 
         return self.service.dict()
