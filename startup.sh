28559cdd700613620b6a993b880423c1307bd490
Martin Fides <fidesmartin@gmail.com>
2023-08-02 21:14:09 +0200

    Add docker compose

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 function main() {
-  docker build -t book_parser:latest .
-  docker run --name book_parser-$RANDOM book_parser:latest | tee output.txt
+  docker-compose build
+  docker-compose run --rm bookstore_parser
 }
 
 main
